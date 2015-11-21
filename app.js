0953fb615e138abd71c10835384745429793f2e4
Parth Desai <desaiparth08@gmail.com>
2015-11-21 18:49:12 +0530

    - Setting up basic things in the app.

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var locations = require('./routes/location');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/location', locations);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+var sendError = function(res, message, stack, code) {
+  res.json({
+    message: message,
+    stack: stack,
+    code: code
+  });
+};
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    switch(err.status) {
+      case 400:
+        sendError(res, 'Bad Request!!', err.stack, 400);
+        break;
+      case 404:
+        sendError(res, 'Not Found!!', null, 404);
+        break;
+      case 500:
+      default:
+        sendError(res, 'Internal server Error!!!', err.stack, 500);
+    }
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+
+  console.error(err.stack);
+
+  res.status(err.status || 500);
+  sendError(res, 'Internal server Error!!', null, 500);
+});
+
+
+module.exports = app;
